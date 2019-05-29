55e0321695729d230cc16a36f51dbd6836da7a56
Dien <tmtdien266@gmail.com>
2019-05-29 23:10:01 +0700

    ...

--- src/layouts/admin/AdminMenuBar.js
+++ src/layouts/admin/AdminMenuBar.js
@@ -35,7 +35,7 @@ class AdminMenuBar extends Component {
             <div className="sidebar-menu">
                 <div className="sidebar-header">
                     <div className="logo">
-                        <a href="index.html" style={{fontSize:'15px'}}><i className="ti-dashboard"/> Trang quản lý</a>
+                        <a href="index.html" style={{fontSize:'15px'}}><i className="ti-dashboard"/><span>  Trang quản lý</span></a>
                     </div>
                 </div>
                 <div className="main-menu">
@@ -45,10 +45,10 @@ class AdminMenuBar extends Component {
                                 LocalStorageManager.getMode() == "OWNER" ?
                                     <ul className="metismenu" id="menu">
                                         <li className="active">
-                                            <a href="/nn-vd" aria-expanded="true"><span><i className="ti-dashboard"/>Vật dụng (Người nhận)</span></a>
+                                            <a href="/nn-vd" aria-expanded="true"><i className="ti-shield"/><span>Vật dụng</span></a>
                                         </li>
                                         <li className="active">
-                                            <a href="/tiec-nn" aria-expanded="true"><span><i className="ti-check-box"/>Quản lý Tiệc (Người nhận)</span></a>
+                                            <a href="/tiec-nn" aria-expanded="true"><i className="ti-check-box"/><span>Quản lý Tiệc</span></a>
                                         </li>
                                     </ul>
                                     :
